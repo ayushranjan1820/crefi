2d41a9bd7e0e2e45beb5f0bb9fe84249c935e335
Ayush Ranjan <ayushranjan11@icloud.com>
2024-03-21 02:24:50 +0530

    COMPLETE

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-// import BTC from '../assets/btc-img.png'
+//import BTC from '../assets/btc-img.png'
 import { FiArrowUpRight, FiArrowDown } from 'react-icons/fi'
 import './Featured.css'
 
@@ -8,7 +8,7 @@ const Featured = () => {
 
     const [data, setData] = useState(null)
 
-    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false'
+    const url = 'https://coingecko.p.rapidapi.com/simple/price'
 
     useEffect(() => {
         axios.get(url).then((response) => {
@@ -169,4 +169,4 @@ const Featured = () => {
     )
 }
 
-export default Featured
+export default Featured;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,8 +9,8 @@ const Hero = () => {
 
                 {/* Left Side */}
                 <div className='left'>
-                    <p>Buy & Sell Crypto 24/7 using your retirement account</p>
-                    <h1>Invest in Cryptocurreny with Your IRA</h1>
+                    <p>Buy & Sell Crypto 24/7</p>
+                    <h1>Invest in Cryptocurreny</h1>
                     <p>Buy, Sell, and store hundreds of cryptocurrencies</p>
                     <div className='input-container'>
                         <input type='email' placeholder='Enter your email' />
